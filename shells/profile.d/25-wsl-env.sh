95e75e49296b425132ab3867a78fd883c10bf000
Douglas Hammond <wizhippo@gmail.com>
2020-09-25 10:16:17 -0400

    improve dns search

--- shells/profile.d/25-wsl-env.sh
+++ shells/profile.d/25-wsl-env.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 if [ ! -z "$WSL_DISTRO_NAME" ] && [ -z "$WSL_HOST_IP" ]; then
+    SEARCH_LIST=$(powershell.exe "(Get-DnsClientGlobalSetting).SuffixSearchList")
+    if [ ! -z "$SEARCH_LIST" ]; then
+        sudo cat /etc/resolv.conf > /tmp/resolv.conf.new
+        sudo sed -i '/^search/d' /tmp/resolv.conf.new
+        sudo echo "search $SEARCH_LIST" >> /tmp/resolv.conf.new
+        sudo mv -f /tmp/resolv.conf.new /etc/resolv.conf
+    fi;
+
     export WSL_HOST_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}')
     export WSL_CLIENT_IP=$(ip -4 addr show eth0 | grep -Po 'inet \K[\d.]+')
     export DISPLAY=$WSL_HOST_IP:0.0
