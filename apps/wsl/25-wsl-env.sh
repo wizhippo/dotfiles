1d39a82586eb59c893ca936170dd9e937d3464c8
Douglas Hammond <wizhippo@gmail.com>
2022-10-12 09:44:35 -0400

    update

--- apps/wsl/25-wsl-env.sh
+++ apps/wsl/25-wsl-env.sh
@@ -14,45 +14,8 @@
 #     sudo mv -f /tmp/resolv.conf.new /etc/resolv.conf
 # fi
 
-HAS_CHANGE="no"
-
-HOST_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}')
-if [ ! -z "HOST_IP" ] && [ "$HOST_IP" != "$WSL_HOST_IP" ]; then
-    export WSL_HOST_IP=$HOST_IP
-    HAS_CHANGE="yes"
-fi
-
-CLIENT_IP=$(ip -4 addr show eth0 | grep -Po 'inet \K[\d.]+')
-if [ ! -z "CLIENT_IP" ] && [ "$CLIENT_IP" != "$WSL_CLIENT_IP" ]; then
-    export WSL_CLIENT_IP=$CLIENT_IP
-    HAS_CHANGE="yes"
-fi
-
-# if [ "$HAS_CHANGE" != "no" ]; then
-#     if [ "$XDG_RUNTIME_DIR" != "/mnt/wslg/runtime-dir" ]; then
-#         if [ ! -d /tmp/.X11-unix ]; then
-#             mkdir -p /tmp/.X11-unix
-#         fi 
-#         if [ ! -x /tmp/.X11-unix/X0.pid ]; then
-#             touch /tmp/.X11-unix/X0.pid
-#         fi
-
-#         local pid
-#         pid=$(cat /tmp/.X11-unix/X0.pid);
-
-#         if ! kill -0 $pid > /dev/null 2>&1; then
-#             rm -f /tmp/.X11-unix/X0
-#             socat -b65536 UNIX-LISTEN:/tmp/.X11-unix/X0,fork,mode=777 VSOCK-CONNECT:2:6000 &
-#             pid="$!"
-#             echo $pid > /tmp/.X11-unix/X0.pid
-#         fi
-        #  export DISPLAY=:0.0
-         # unable to create keyring use below
-#         dbus-update-activation-environment --systemd DISPLAY         
-#         # export DISPLAY=$WSL_HOST_IP:0.0
-#         powershell.exe -ExecutionPolicy Bypass -File $HOME/.dotfiles/apps/wsl/wsl-x410.ps1 -HostIP $WSL_HOST_IP -ClientIP $WSL_CLIENT_IP
-#     fi
-# fi
+export WSL_HOST_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}')
+export WSL_CLIENT_IP=$(ip -4 addr show eth0 | grep -Po 'inet \K[\d.]+')
 
 # https://github.com/rupor-github/win-gpg-agent
 # win-gpg-agent should be running first from windows
@@ -82,11 +45,13 @@ if [ command -v wslview &> /dev/null ]; then
     export BROWSER=wslview
 fi
 
-export DISPLAY=:0.0
-
-# somtimes dbus is not up yet from sytemd, wait for it
-NEXT_WAIT_TIME=0
-until [ $NEXT_WAIT_TIME -eq 5 ] || pgrep -u $UID dbus > /dev/null; do
-    sleep $(( NEXT_WAIT_TIME++ ))
-done
-[ $NEXT_WAIT_TIME -lt 5 ] && dbus-update-activation-environment --systemd --all
+if [ "$XDG_RUNTIME_DIR" != "/mnt/wslg/runtime-dir" ]; then
+    export DISPLAY=:0.0
+    # somtimes dbus is not up yet from sytemd, wait for it
+    local NEXT_WAIT_TIME=0
+    until [ $NEXT_WAIT_TIME -eq 5 ] || pgrep -u $UID dbus > /dev/null; do
+        sleep $(( NEXT_WAIT_TIME++ ))
+    done
+    [ $NEXT_WAIT_TIME -lt 5 ] && dbus-update-activation-environment --systemd --all
+    unset $NEXT_WAIT_TIME
+fi
