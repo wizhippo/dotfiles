9ba8cd307114ca74bef6c4902b1576d5588d937c
Douglas Hammond <wizhippo@gmail.com>
2023-03-24 11:04:23 -0400

    update

--- apps/wsl/25-wsl-env.sh
+++ apps/wsl/25-wsl-env.sh
@@ -2,44 +2,8 @@
 
 [ ! -z "$WSL_DISTRO_NAME" ] || return 0
 
-# HOST_NS_SEARCH_LIST=$(powershell.exe -Command '$list = (Get-DnsClient).ConnectionSpecificSuffix; [system.String]::Join(" ", $list)' | tr -d '\r' | xargs)
-# NS_SEARCH_LIST=$(cat /etc/resolv.conf | grep search | awk '{print $2; exit;}')
-
-# if [ "$HOST_NS_SEARCH_LIST" != "$NS_SEARCH_LIST" ]; then
-#     sudo cat /etc/resolv.conf >/tmp/resolv.conf.new
-#     sudo sed -i '/^search/d' /tmp/resolv.conf.new
-#     if [ ! -z "$HOST_NS_SEARCH_LIST" ]; then
-#         sudo echo "search $HOST_NS_SEARCH_LIST" >>/tmp/resolv.conf.new
-#     fi
-#     sudo mv -f /tmp/resolv.conf.new /etc/resolv.conf
-# fi
-
 export WSL_HOST_IP=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}')
 export WSL_CLIENT_IP=$(ip -4 addr show eth0 | grep -Po 'inet \K[\d.]+')
-
-# https://github.com/rupor-github/win-gpg-agent
-# win-gpg-agent should be running first from windows
-
-# detect what we have
-# if [  $(uname -a | grep -c "Microsoft") -eq 1 ]; then
-#     export ISWSL=1 # WSL 1
-# elif [ $(uname -a | grep -c "microsoft") -eq 1 ]; then
-#    export ISWSL=2 # WSL 2
-# else
-#     export ISWSL=0
-# fi
-# if [ ${ISWSL} -eq 1 ]; then
-#     # WSL 1 could use AF_UNIX sockets from Windows side directly
-#     if [ -n ${WSL_AGENT_HOME} ]; then
-#         export GNUPGHOME=${WSL_AGENT_HOME}
-#         export SSH_AUTH_SOCK=${WSL_AGENT_HOME}/S.gpg-agent.ssh
-#     fi
-# elif [ ${ISWSL} -eq 2 ]; then
-#     # ${HOME}/.local/bin/win-gpg-agent-relay start
-#     export SSH_AUTH_SOCK=${HOME}/.gnupg/S.gpg-agent.ssh
-# else
-#     echo "Unkonwn WSL version"
-# fi
 
 if [ command -v wslview &> /dev/null ]; then
     export BROWSER=wslview
@@ -60,7 +24,3 @@ if pgrep -a -u $UID gnome-keyring-d >/dev/null; then
     export GNOME_KEYRING_CONTROL=/run/user/$UID/keyring
     export SSH_AUTH_SOCK=/run/user/$UID/keyring/ssh
 fi
-
-# see https://github.com/microsoft/wslg/issues/715
-#export MESA_D3D12_DEFAULT_ADAPTER_NAME="llvm"
-#export LIBGL_ALWAYS_SOFTWARE=true
--- shells/sh.d/10-env.sh
+++ shells/sh.d/10-env.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
-fi
-
 if [ -d "$HOME/.local/share/JetBrains/Toolbox/scripts" ] ; then
     PATH="$HOME/.local/share/JetBrains/Toolbox/scripts:$PATH"
 fi
@@ -12,12 +8,14 @@ if [ -d "$HOME/.local/bin" ] ; then
     PATH="$HOME/.local/bin:$PATH"
 fi
 
-if [ -e $HOME/.local/bin/edit ]; then
-    export EDITOR=$HOME/.local/bin/edit
-fi
-
 [ -z "$TMUX" ] || export FZF_TMUX=1
 
 if [ -e $HOME/.local/bin/vault-env ]; then
     export ANSIBLE_VAULT_PASSWORD_FILE=$HOME/.local/bin/vault-env
+fi
+
+if [ -e $HOME/.npm ]; then
+    export NPM_CONFIG_PREFIX="${HOME}/.npm"
+    export PATH="$PATH:$NPM_CONFIG_PREFIX/bin"
+    export MANPATH="${MANPATH-$(manpath)}:$NPM_CONFIG_PREFIX/share/man"
 fi
