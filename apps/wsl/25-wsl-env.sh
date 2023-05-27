f7892da0d590c6bf4e8b3156cbac4358da447ee2
Douglas Hammond <wizhippo@gmail.com>
2023-05-26 22:48:58 -0400

    Update

--- apps/wsl/25-wsl-env.sh
+++ apps/wsl/25-wsl-env.sh
@@ -17,10 +17,30 @@ if [ "$XDG_RUNTIME_DIR" != "/mnt/wslg/runtime-dir" ]; then
         sleep $(( NEXT_WAIT_TIME++ ))
     done
     [ $NEXT_WAIT_TIME -lt 5 ] && dbus-update-activation-environment --systemd --all
-    unset $NEXT_WAIT_TIME
+    unset NEXT_WAIT_TIME
 fi
 
 if pgrep -a -u $UID gnome-keyring-d >/dev/null; then
     export GNOME_KEYRING_CONTROL=/run/user/$UID/keyring
     export SSH_AUTH_SOCK=/run/user/$UID/keyring/ssh
 fi
+
+
+
+wslg_dpi_scale() {
+    local dpi_scale dpi
+    dpi_scale="${GDK_DPI_SCALE:-${QT_SCALE_FACTOR:-}}"
+    if [[ -z "${dpi_scale:-}" ]] ; then
+        dpi=$("/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe" -Command "\$wmi = Get-WmiObject -Class Win32_DesktopMonitor -Namespace 'root\\CIMV2' -Filter 'Availability = 3'; \$dpi = \$wmi.PixelsPerXLogicalInch; Write-Output \$dpi" | tr -d '\r\n')
+        dpi_scale=$(bc <<<"scale=2; $dpi / 96")
+    fi
+
+    export GDK_DPI_SCALE=${GDK_DPI_SCALE:-$dpi_scale}
+    export GTK_SCALE=${GTK_SCALE:-$dpi_scale}
+    export QT_SCALE_FACTOR=${QT_SCALE_FACTOR:-$GDK_DPI_SCALE}
+
+    echo "DPI: $dpi, Scale: $dpi_scale, GDK_DPI_SCALE: $GDK_DPI_SCALE, GTK_SCALE: $GTK_SCALE, QT_SCALE_FACTOR: $QT_SCALE_FACTOR"
+}
+
+
+[ -d /mnt/wslg/runtime-dir ] && wslg_dpi_scale
--- shells/sh.d/10-env.sh
+++ shells/sh.d/10-env.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
+[ -z "$TMUX" ] || export FZF_TMUX=1
+
 if [ -d "$HOME/.local/share/JetBrains/Toolbox/scripts" ] ; then
     PATH="$HOME/.local/share/JetBrains/Toolbox/scripts:$PATH"
 fi
-
-if [ -d "$HOME/.local/bin" ] ; then
-    PATH="$HOME/.local/bin:$PATH"
-fi
-
-[ -z "$TMUX" ] || export FZF_TMUX=1
 
 if [ -e $HOME/.local/bin/vault-env ]; then
     export ANSIBLE_VAULT_PASSWORD_FILE=$HOME/.local/bin/vault-env
 fi
 
-if [ -e $HOME/.npm ]; then
-    export NPM_CONFIG_PREFIX="${HOME}/.npm"
-    export PATH="$PATH:$NPM_CONFIG_PREFIX/bin"
-    export MANPATH="${MANPATH-$(manpath)}:$NPM_CONFIG_PREFIX/share/man"
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
 fi
